a547a56bd2dc702846531a6008337f2534ed97fa
Piyush Harsh <piyush.harsh@zhaw.ch>
2014-09-04 14:30:20 +0200

    minor script changes, silent wgets

--- src/script.sh
+++ src/script.sh
@@ -18,14 +18,17 @@ apt-get install -y foreman-installer
 rm /etc/foreman/foreman-installer-answers.yaml
 cp /tmp/files-to-go/foreman-installer-answers.yaml /etc/foreman/foreman-installer-answers.yaml
 foreman-installer
-# cp /tmp/files-to-go/default /var/lib/tftpboot/pxelinux.cfg/
+# copy the discovery template in correct folder
+cp /tmp/files-to-go/default /var/lib/tftpboot/pxelinux.cfg/
 cd /tmp/
-wget http://downloads.theforeman.org/discovery/releases/latest/foreman-discovery-image-3.0.5-20140523.0.el6.iso-vmlinuz
-wget http://downloads.theforeman.org/discovery/releases/0.5/foreman-discovery-image-3.0.5-20140523.0.el6.iso-img
+echo "Downloading discovery images and linux netboot files. Please be patient ..."
+wget --quiet http://downloads.theforeman.org/discovery/releases/latest/foreman-discovery-image-3.0.5-20140523.0.el6.iso-vmlinuz
+wget --quiet http://downloads.theforeman.org/discovery/releases/0.5/foreman-discovery-image-3.0.5-20140523.0.el6.iso-img
 mv /tmp/foreman-discovery-image-3.0.5-20140523.0.el6.iso-vmlinuz /var/lib/tftpboot/boot/vmlinuz0
 mv /tmp/foreman-discovery-image-3.0.5-20140523.0.el6.iso-img /var/lib/tftpboot/boot/initrd0.img
-wget --timeout=10 --tries=3 --no-check-certificate -nv -c "http://archive.ubuntu.com/ubuntu//dists/trusty/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/initrd.gz" -O "/var/lib/tftpboot/boot/Ubuntu-12.04-x86_64-initrd.gz"
-wget --timeout=10 --tries=3 --no-check-certificate -nv -c "http://archive.ubuntu.com/ubuntu//dists/trusty/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/linux" -O "/var/lib/tftpboot/boot/Ubuntu-12.04-x86_64-linux"
+echo "Now downloading the ubuntu images ..."
+wget --timeout=10 --tries=3 --quiet --no-check-certificate -nv -c "http://archive.ubuntu.com/ubuntu//dists/trusty/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/initrd.gz" -O "/var/lib/tftpboot/boot/Ubuntu-12.04-x86_64-initrd.gz"
+wget --timeout=10 --tries=3 --quiet --no-check-certificate -nv -c "http://archive.ubuntu.com/ubuntu//dists/trusty/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/linux" -O "/var/lib/tftpboot/boot/Ubuntu-12.04-x86_64-linux"
 /sbin/iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE
 /sbin/iptables --append FORWARD --in-interface eth1 -j ACCEPT
 sysctl net.ipv4.ip_forward=1
